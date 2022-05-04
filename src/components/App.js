24dbe1f63e72287fa8880eef7ca2aa4f97a1039b
Carmenyo <carmencastroyo@gmail.com>
2022-05-04 15:23:14 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+// import sirve para enlazar /traer las dependencias que necesitamos.
+// Librerías JS
+// CSS
+// Imágenes
+// JSON
+
+import '../styles/App.scss';
+
+
+// Función principal.
+function App() {
+  // Variables generales, constantes, variables de estado.
+  // Hooks.
+  // Funciones genrales, funnciones manejadoras de eventos.
+
+  return (
+    // Como solo se puede devolver una etiqueta, se puede usar una etiqueta ficticia vacia, llamada FRAGMENTO, y que se escribe, abiriendo y cerrando la etiqueta vacia, así "<> html </>".
+    <>
+      <header className='header'>
+  
+      </header>
+    </>
+  );
+}
+
+export default App;
