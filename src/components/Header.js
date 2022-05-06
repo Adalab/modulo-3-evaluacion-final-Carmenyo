d6afcfd0386bff0082fd0e2e8321a529a4b20ed6
Carmenyo <carmencastroyo@gmail.com>
2022-05-06 16:02:04 +0200

    Publish in GitHub Pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { AiFillGithub  } from "react-icons/ai";
 const Header = () => {
   return <header className="header"><img src= {logo} alt="" className="header__logo"></img>
   
-  <a href="http://beta.adalab.es/modulo-3-evaluacion-final-Carmenyo/"> <AiFillGithub className="header__icon"/>
+  <a href="https://github.com/Adalab/modulo-3-evaluacion-final-Carmenyo" target="_blank"> <AiFillGithub className="header__icon"/>
   </a>
 
    </header>
